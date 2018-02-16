16edcaa6886c7ba7c49c8856f5938bcc634470cc
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-02-16 05:13:33 +0000

    Fixed

--- pocketchef/serverRestart.sh
+++ pocketchef/serverRestart.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 arg1=$1
-if ["$arg1" = "--help"]
+if [ "$arg1" = "--help" ]
 then
   echo "Hello new friend!"
-  echo "To start the server use ./serverRestart.sh"
-  echo "To kill the server use ./serverRestart.sh --kill"
-  echo "And of course, to access this page use ./serverRestart --help"
+  echo "To start the server:"
+  echo "	./serverRestart.sh"  
+  echo "To kill the server:"
+  echo "	./serverRestart.sh --kill"
+  echo "And of course, to access this page:"
+  echo "	./serverRestart.sh --help"
 else
   sudo docker ps > info.txt
   awk '{print $1}' info.txt > num.txt
@@ -38,6 +41,6 @@ else
         sleep 10s
         echo -e "              0000_____________0000________0000000000000000__000000000000000000+\n            00000000_________00000000______000000000000000__0000000000000000000+\n           000____000_______000____000_____000_______0000__00______0+\n          000______000_____000______000_____________0000___00______0+\n         0000______0000___0000______0000___________0000_____0_____0+\n         0000______0000___0000______0000__________0000___________0+\n         0000______0000___0000______0000_________000___0000000000+\n         0000______0000___0000______0000________0000+\n          000______000_____000______000________0000+\n           000____000_______000____000_______00000+\n            00000000_________00000000_______0000000+\n              0000_____________0000________000000007;"
         sleep 50s
-        echo "Done. Men only want one thing and it's disgusting"
+        echo "Done"
   fi
 fi

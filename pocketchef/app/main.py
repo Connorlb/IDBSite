a44eed366cef2c7f34e6283aca3a509d907e375a
Aditya Prasad <aprasad@cs.utexas.edu>
2018-02-15 19:56:13 -0600

    Added html files for specific pages

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Main HTML pages
 @app.route("/")
 def home() :
     return render_template('home.html')
@@ -22,9 +23,47 @@ def recipes() :
 def about() :
     return render_template('about.html')
 
+
+# Restaurant Pages
 @app.route("/restaurants/maggianos")
 def maggianos() :
-    return render_template('maggianos.html')  
+    return render_template('maggianos.html')
+
+@app.route("/restaurants/cabobobs")
+def cabobobs() :
+    return render_template('cabobobs.html')
+
+@app.route("/restaurants/cheesecakefactory")
+def cheesecakefactory() :
+    return render_template('cheesecakefactory.html')  
+
+
+# Recipe Pages
+@app.route("/recipes/smores")
+def smores() :
+    return render_template('smores.html')
+
+@app.route("/recipes/steakburrito")
+def steakburrito() :
+    return render_template('steakburrito.html')
+
+@app.route("/recipes/tomatosoup")
+def tomatosoup() :
+    return render_template('tomatosoup.html')
+
+
+# Ingredient Pages
+@app.route("/ingredients/grahamcrackers")
+def grahamcrackers() :
+    return render_template('grahamcrackers.html')
+
+@app.route("/ingredients/tomatoes")
+def tomatoes() :
+    return render_template('tomatoes.html')
+
+@app.route("/ingredients/tortillas")
+def tortillas() :
+    return render_template('tortillas.html')  
 
 if __name__ == "__main__" :
     app.run(host='0.0.0.0', debug=True, port=80)

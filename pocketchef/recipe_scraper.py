fc71d52513d9e8fc65b9e9b882160aef835533f6
root <aprasad101@utexas.edu>
2018-03-16 20:40:36 +0000

    Added simple scraper

---
+++ pocketchef/recipe_scraper.py
@@ -0,0 +1,20 @@
+import urllib.request
+import json
+
+# Request Headers
+api_key = "eSpzDnsnCKmshP9N9IbybaCNXlQsp1P3BQSjsn0xgRvKJA2hrE"
+accept = "application/json"
+
+# Parameters
+title = "title=tomato+soup"
+
+# GET Request
+html = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?" + title
+req = urllib.request.Request(html, headers={"X-Mashape-Key":api_key, "Accept":accept})
+data = urllib.request.urlopen(req)
+data_str = str(data.read())
+data_str = data_str[2 : len(data_str)-1]
+
+j_data = json.loads(data_str)
+
+print(str(j_data['calories']['value']) + " calories")

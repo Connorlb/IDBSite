fa66d12b6b04dd655f8642c99748ad25c1879b65
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-03-20 08:05:52 +0000

    Front-End: babel-loader TypeError: cannot read property 'babel'

---
+++ pocketchef-2/webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const config = {
+    entry:  __dirname + '/src/index.js',
+    mode: 'development',
+    output: {
+        path: __dirname + '/static',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        rules: [
+    	   {
+       	    test: /\.jsx?/,
+      	    exclude: /node_modules/,
+    	    use: 'babel-loader'
+    	   }
+  	]
+    },
+};
+module.exports = config;

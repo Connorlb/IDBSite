601700b54e231b92fbb34db246ab6b3386cef07e
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-03-20 16:48:58 +0000

    F-E: Webpack Troubles Continue

--- pocketchef-2/webpack.config.js
+++ pocketchef-2/webpack.config.js
@@ -1,22 +1,25 @@
 const webpack = require('webpack');
-const config = {
-    entry:  __dirname + '/src/index.js',
-    mode: 'development',
-    output: {
-        path: __dirname + '/static',
-        filename: 'bundle.js',
+
+module.exports = {
+entry: [
+  './src/index.js'
+],
+output: {
+  path: __dirname + '/static',
+  filename: 'bundle.js',
+},
+module: {
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+	presets: ['es2015', 'react']
+      }
     },
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    module: {
-        rules: [
-    	   {
-       	    test: /\.jsx?/,
-      	    exclude: /node_modules/,
-    	    use: 'babel-loader'
-    	   }
-  	]
-    },
+  ],
+},
+plugins: [
+]
 };
-module.exports = config;

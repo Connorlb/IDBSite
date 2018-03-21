0ce9c4a74dc78ddfc71ff394ea5a9a21c0727567
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-03-21 00:08:13 +0000

    F-E: Server Setup Again

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Oh, Hello World"
+def index():
+    return render_template('index.html');
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)

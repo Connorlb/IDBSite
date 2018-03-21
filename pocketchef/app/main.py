eee31548a833723d7d3833b209d0b95082fa3c9f
Matthew Curtis <matthewcurtis@Matthews-MBP.austin.rr.com>
2018-03-20 23:17:26 -0500

    Finally working locally

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 # the all-important app variable:
-app = Flask(__name__, static_folder="static", template_folder="public")
+app = Flask(__name__, static_folder="../static", template_folder="../public")
 
 @app.route("/")
 def index():
@@ -20,4 +20,5 @@ def hello():
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    #app.run(host='0.0.0.0', debug=True, port=80)
+    app.run()

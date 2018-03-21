f6ae8f46bff68821d2a15a9bba4ea015ab3aa510
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-03-21 04:19:05 +0000

    zomato running

--- db_interface.py
+++ db_interface.py
@@ -32,15 +32,17 @@ try:
     cur.execute(sql)
     rows = cur.fetchall()
     for row in rows :
-        url = get_req + q + strip_punctuation(row[0].replace(' ', '%20')) + lat + str(row[1]) + lon + str(row[2])
+        url = get_req + q + strip_punctuation(row[0]).replace(' ', '%20') + lat + str(row[1]) + lon + str(row[2])
         req = urllib.request.Request(url, headers={"user-key":api_key, "Accept":accept})
         data = urllib.request.urlopen(req)
         j_data = json.loads(data.read().decode("utf-8"))
-        if j_data["results_shown"] > 0 :
-            print(j_data["restaurants"][0]["id"] + " " + j_data["restaurants"][0]["name"])
+ #       print(json.dumps(j_data,indent=4,sort_keys=True))
+        #if j_data["results_shown"] > 0 :
+        print(str(j_data["restaurants"][0]["restaurant"]["R"]["res_id"]) + " " + j_data["restaurants"][0]["restaurant"]["name"])
     cur.close()
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 finally:
     if conn is not None :
         conn.close()
+

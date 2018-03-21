abc520669b21d7449e9aefba421763eab9713409
Matthew Curtis <matthewcurtis@Matthews-MBP.austin.rr.com>
2018-03-20 21:41:54 -0500

    Incr change

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
 
 # the all-important app variable:
-app = Flask(__name__, static_folder="../static", template_folder=".")
+app = Flask(__name__, static_folder="../static", template_folder="../public")
 
 @app.route("/")
 def index():
-    return render_template('index.html');
+    return render_template('../app/index.html');
 
-@app.route("/index1")
+@app.route("/home")
 def index1():
-    return render_template('pocketchef/public/index.html');
+    return render_template('home.html');
 
-@app.route("/index2")
+@app.route("/index")
 def index2():
-    return render_template('public/index.html');
+    return render_template('index.html');
 
 @app.route("/hello")
 def hello():

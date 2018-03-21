529dc2f547bada1600053ebb4b7ecdbf25932d8d
dalisay-pat <dalisay.pat@utexas.edu>
2018-03-21 18:02:06 -0500

    we back on the carousel

--- pocketchef/src/components/Home.jsx
+++ pocketchef/src/components/Home.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap' ;
+import { Jumbotron, Grid, Row, Col, Image, Button , Carousel} from 'react-bootstrap' ;
 import './Home.css'
 
 export default class Home extends Component {
   render(){
     return (
       <Grid>
-        <Jumbotron>
-          <h2> Welcome to PocketChef </h2>
-          <p> Bringing restaurants to your home </p>
-        </Jumbotron>
+        <Carousel>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src="../public/assets/restaurant_carousel.png" />
+            <Carousel.Caption>
+              <h3>Restaurants</h3>
+              <p>Find a restaurant to satisfy your cravings.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src="../public/assets/recipe_carousel.png" />
+            <Carousel.Caption>
+              <h3>Recipes</h3>
+              <p>Find a recipe for your favorite foods.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width={900} height={500} alt="900x500" src="../public/assets/ingredients_carousel.png" />
+            <Carousel.Caption>
+              <h3>Ingredients</h3>
+              <p>Find the best ingredients for your meals.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>;
       </Grid>
     );
   }

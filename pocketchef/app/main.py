0c902a17630bb8e76ca6c0cec31abcad04c9c0f5
Jesus Romero <ejesusromero@gmail.com>
2018-03-21 16:13:08 -0500

    F-E: Cleared main.py

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -7,18 +7,6 @@ app = Flask(__name__, static_folder="../static", template_folder="../public")
 def index():
     return render_template('index.html');
 
-@app.route("/home")
-def index1():
-    return render_template('home.html');
-
-@app.route("/index")
-def index2():
-    return render_template('index.html');
-
-@app.route("/hello")
-def hello():
-    return "Hello World"
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
     #app.run()

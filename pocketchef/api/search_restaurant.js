98ef51236ab2f0d164a0ee15e7ac1de78121fef9
Aditya Prasad <aprasad101@utexas.edu>
2018-03-22 17:20:17 -0500

    reorganized directories

--- pocketchef/api/search_restaurant.js
+++ pocketchef/api/search_restaurant.js
@@ -3,9 +3,8 @@ var pg = require('pg');
 var connectionString = "postgres://postgres:pocketchef@localhost/pocketchef";
 var pgClient = new pg.Client(connectionString);
 pgClient.connect();
-pgClient.query("SELECT name FROM api.restaurants WHERE cuisine='American'", (err, res) =>{
+pgClient.query("SELECT name, rating FROM api.restaurants WHERE cuisine='American'", (err, res) => {
     if (err) throw err
-    console.log(JSON.stringify(res,null,2))    
+    console.log(JSON.stringify(res, null, 2))
     pgClient.end()
 })
-console.log("RIP");

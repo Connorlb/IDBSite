a86c3b7bed27f06179c5ceb6aca39cdd8335dc0d
Aditya Prasad <aprasad101@utexas.edu>
2018-03-22 00:14:58 -0500

    Working on ingr_parser.py

--- scrapers/formatter.py
+++ scrapers/formatter.py
@@ -1,3 +1,9 @@
+# Strips all punctuation from a string
+def strip_punctuation(s) :
+    return ''.join(c for c in s if c not in punctuation)
+cuisine_list = set()
+
+# Formats recipeIDs into a list of tuples
 def formatter(s) :
     result = []
     with open(s) as f :
@@ -9,3 +15,11 @@ def formatter(s) :
                 s[1][i] = int(s[1][i])
             result += (s,)
     return result
+
+# Formats ingredient string into a list of tuples
+def ingredient_formatter(s) :
+    result = []
+    out = s.split('@@')
+    for i in range(len(out)):
+        out[i] = out[i].split('::')
+    return out

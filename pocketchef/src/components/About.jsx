a8966c8f6f2501d5670f3212a91301550f87ac9e
Jesus Romero <ejesusromero@gmail.com>
2018-03-21 23:53:26 -0500

    F-E: About changes-5

--- pocketchef/src/components/About.jsx
+++ pocketchef/src/components/About.jsx
@@ -18,7 +18,7 @@ export default class About extends Component {
           <h2 align="center">Team SWEats</h2>
         </Row>
         <Row align= "center">
-          <Col xs={12} sm={4} className="image-wrap" align="center">
+          <Col xs={12} sm={4}>
             <Image width={200} height={200} alt="200x200" src="https://i.imgur.com/9iA21IL.jpg" circle align="center"/>
             <h3>Aditya Prasad</h3>
             <li class="list-group-item"><b>About:</b> Aditya is a third-year CS student who is super into playing instruments, but not one specific one. He plays four different ones. He really enjoys going to Halal Bros.</li>
@@ -27,7 +27,7 @@ export default class About extends Component {
             <li class="list-group-item"><b>Issues:</b>0</li>
             <li class="list-group-item"><b>Unit Tests:</b>0</li>
           </Col>
-          <Col xs={12} sm={4} className="image-wrap" align="center">
+          <Col xs={12} sm={4}>
             <Image width={200} height={200} alt="200x200" src="https://i.imgur.com/bXpurgB.jpg" circle align="center"/>
             <h3>Connor Burkman</h3>
             <li class="list-group-item"><b>About:</b> Connor is a fourth-year CS student who thinks Panda Express is too spicy. He's in love with some of the food at Song La.</li>
@@ -36,7 +36,7 @@ export default class About extends Component {
             <li class="list-group-item"><b>Issues:</b>0</li>
             <li class="list-group-item"><b>Unit Tests:</b>0</li>
           </Col>
-          <Col xs={12} sm={4} className="image-wrap" align="center">
+          <Col xs={12} sm={4}>
             <Image width={200} height={200} alt="200x200" src="https://i.imgur.com/puiutQZ.jpg" circle align="center"/>
             <h3>Jesus Romero</h3>
             <li class="list-group-item"><b>About:</b> Jesus is a fourth-year CS student who loves tamales. He's an advocate of Olive Garden and splurges at Sushi Junai.</li>
@@ -47,7 +47,7 @@ export default class About extends Component {
           </Col>
         </Row>
         <Row align= "center">
-          <Col xs={12} sm={4} className="image-wrap" align="center">
+          <Col xs={12} sm={4} xsOffset={4}>
             <Image width={200} height={200} alt="200x200" src="https://i.imgur.com/H158r6O.jpg" circle align="center"/>
             <h3>Matthew Curtis</h3>
             <li class="list-group-item"><b>About:</b> Matthew is a fourth-year CS students who has the misfortune of having first names for both first and last names. He loves the take on Asian cuisine at the local food truck Pinch.</li>
@@ -56,7 +56,7 @@ export default class About extends Component {
             <li class="list-group-item"><b>Issues:</b>18</li>
             <li class="list-group-item"><b>Unit Tests:</b>0</li>
           </Col>
-          <Col xs={12} sm={4} className="image-wrap" align="center">
+          <Col xs={12} sm={4}>
             <Image width={200} height={200} alt="100x100" src="https://i.imgur.com/BmRVX02.jpg" circle align="center"/>
             <h3>Patrick Dalisay</h3>
             <li class="list-group-item"><b>About:</b> Patrick is a third-year CS student who enjoys long walks on the beach and competitive dancing. His favorite place to eat is at basically any taco truck that serves some nice, cheap tacos al pastor.</li>

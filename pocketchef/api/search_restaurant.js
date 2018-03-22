f5487101acf89ae57cdaa8b779afa39feb100489
Aditya Prasad <aprasad101@utexas.edu>
2018-03-22 17:57:37 -0500

    js database querying

--- pocketchef/api/search_restaurant.js
+++ pocketchef/api/search_restaurant.js
@@ -1,12 +1,27 @@
 var pg = require('pg');
 
 var connectionString = "postgres://postgres:pocketchef@localhost/pocketchef";
-var pgClient = new pg.Client(connectionString);
-pgClient.connect();
-var data = pgClient.query("SELECT name, rating FROM api.restaurants WHERE cuisine='American'", (err, res) => {
-    if (err) throw err;
-    out = JSON.stringify(res);
-    pgClient.end();
-    return out;
-});
-console.log(data)
+var client = new pg.Client(connectionString);
+client.connect();
+// var data = pgClient.query("SELECT name, rating FROM api.restaurants WHERE cuisine='American'", (err, res) => {
+//     if (err) throw err;
+//     out = JSON.stringify(res);
+//     pgClient.end();
+//     return out;
+// });
+// console.log(data)
+const query = {
+    text: 'SELECT $1::text, $2::text FROM $3::text',
+    values: ['name', 'rating', 'restaurants'],
+    rowMode: 'array'
+};
+
+client.query(query, (err, res) => {
+    if (err) {
+        console.log(err.stack)
+    }
+    else {
+        console.log(res.felds.map(f => field.name))
+        console.log(res.rows[0])
+    }
+})

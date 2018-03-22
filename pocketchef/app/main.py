8ef900d295a6e118336a73ef2ff7ade6c00f1a61
Jesus Romero <ejesusromero@gmail.com>
2018-03-21 20:27:37 -0500

    F-E: Routing -2

--- pocketchef/app/main.py
+++ pocketchef/app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder="../static", template_folder="../public")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def index():
+def index(path):
     return render_template('index.html');
 
 if __name__ == "__main__":

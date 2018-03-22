32cbbbd0020499f9d261d0c24136fbea72dd1e4e
Aditya Prasad <aprasad101@utexas.edu>
2018-03-22 17:33:22 -0500

    modified api

--- pocketchef/api/search_restaurant.js
+++ pocketchef/api/search_restaurant.js
@@ -1,11 +1,12 @@
 var pg = require('pg');
 
+var data = ""
 var connectionString = "postgres://postgres:pocketchef@localhost/pocketchef";
 var pgClient = new pg.Client(connectionString);
 pgClient.connect();
 pgClient.query("SELECT name, rating FROM api.restaurants WHERE cuisine='American'", (err, res) => {
     if (err) throw err
-    // console.log(JSON.stringify(res, null, 2))
-    console.log(typeof res)
+    data = JSON.stringify(res)
     pgClient.end()
 })
+console.log(data)

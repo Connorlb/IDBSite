b14a0619dd55b15b201080c435907e9c3037fbd8
Ubuntu <ubuntu@ip-172-31-45-109.us-east-2.compute.internal>
2018-03-22 01:59:26 +0000

    edited cuisine list

--- scrapers/spoons.py
+++ scrapers/spoons.py
@@ -1,52 +1,38 @@
 import urllib.request
 import html
 import json
-import psycopg2
 import unidecode
 from string import punctuation
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-
 # Helper Functions
 def strip_punctuation(s) :
     return ''.join(c for c in s if c not in punctuation)
-cuisine_list = set()
 
-# Database Connection
-conn = psycopg2.connect("dbname=pocketchef user=postgres")
-conn.autocommit = True
-cur = conn.cursor()
+cuisines = ['Pizza', 'Cheese', 'Cafes', 'Ethiopian', 'Vietnamese', 'Cuban', 'Bars', 'Tapas/Small Plates', 'Asian Fusion', 'Seafood', 'American', 'Barbeque', 'Brazilian', 'European', 'Caribbean', 'Ramen', 'Spanish', 'Bagels', 'Diners', 'Mexican', 'Coffee & Tea', 'Tex-Mex', 'Desserts', 'Chicken Wings', 'Lounges', 'Delis', 'Cajun/Creole', 'Tapas Bars', 'Wine Bars', 'Burgers', 'Thai', 'Latin American', 'Donuts', 'Vegan', 'Southern', 'Middle Eastern', 'Sandwiches', 'Italian', 'Gastropubs', 'Food Trucks', 'Poke', 'Sushi Bars', 'Cocktail Bars', 'Chinese', 'Indian', 'Beer Bar', 'Bakeries', 'Greek', 'French', 'Hot Dogs', 'Beer Gardens', 'Breakfast & Brunch', 'Pubs', 'Mediterranean', 'Dim Sum', 'Gluten-Free', 'Vegetarian', 'Japanese', 'Food Stands', 'Korean', 'German', 'Juice Bars & Smoothies', 'Steakhouses']
 
 # Request Headers
-api_key = "21b7ae1762dcd719479c76abbd77c7ad"
+api_key = "eSpzDnsnCKmshP9N9IbybaCNXlQsp1P3BQSjsn0xgRvKJA2hrE"
 accept = "application/json"
 
 # Parameters
-get_req = "https://developers.zomato.com/api/v2.1/search?"
-q = "q="
-lat = "&lat="
-lon = "&lon="
+get_req = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?"
 
-# SQL Query
-sql = """SELECT cuisine FROM restaurants"""
+# List of cuisines to iterate over
 
 # Request
-try:
-    cur.execute(sql)
-    rows = cur.fetchall()
-    for row in rows :
-        cuisine_list.add(row[0])
- #        url = get_req + q + strip_punctuation(unidecode.unidecode(row[0])).replace(' ', '%20') + lat + str(row[1]) + lon + str(row[2])
- #        req = urllib.request.Request(url, headers={"user-key":api_key, "Accept":accept})
- #        data = urllib.request.urlopen(req)
- #        j_data = json.loads(data.read().decode("utf-8"))
- # #       print(json.dumps(j_data,indent=4,sort_keys=True))
- #        if j_data["results_shown"] > 0 :
+for cuis in cuisines :
+     url = get_req + "cuisine=" + strip_punctuation(unidecode.unidecode(cuis)).replace(' ', '%20')
+     req = urllib.request.Request(url, headers={"X-Mashape-Key":api_key, "Accept":accept})
+     data = urllib.request.urlopen(req)
+     j_data = json.loads(data.read().decode("utf-8"))
+     temp = str(cuis) + ":"
+     i = 0
+     for res in j_data["results"]:
+         temp = temp + " " + str(res["id"])
+         i+=1
+         if i > 7:
+             break
+     print(temp)
+		
+ #       print(json.dumps(j_data,indent=4,sort_keys=True))
+  #       if j_data["results_shown"] > 0 :
  #            print(str(j_data["restaurants"][0]["restaurant"]["R"]["res_id"]) + " " + j_data["restaurants"][0]["restaurant"]["name"])
-    cur.close()
-    print(cuisine_list)
-    print(len(cuisine_list))
-except (Exception, psycopg2.DatabaseError) as error:
-    print(error)
-finally:
-    if conn is not None :
-        conn.close()

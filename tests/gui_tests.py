f43ece48983739caa23107c442c323e65fde2dd6
Connorlb <connor.burkman@gmail.com>
2018-03-22 21:40:08 -0500

    selenium tests done

--- tests/gui_tests.py
+++ tests/gui_tests.py
@@ -1,7 +1,8 @@
 import unittest
+import time
 from selenium import webdriver
 
-class TestHome(unittest.TestCase) :
+class Test1(unittest.TestCase) :
     def setUp(self) :
         self.browser = webdriver.Firefox()
 
@@ -10,6 +11,21 @@ class TestHome(unittest.TestCase) :
         element = self.browser.find_element_by_tag_name('title')
         text = element.get_attribute("innerText")
         self.assertEqual(text, 'PocketChef')
+
+    def tearDown(self) :
+        self.browser.quit()
+
+class Test2(unittest.TestCase) :
+    def setUp(self) :
+        self.browser = webdriver.Firefox()
+
+    def testLink(self) :
+        self.browser.get('http://www.pocketchef.me/restaurants')
+        element = self.browser.find_element_by_class_name('navbar-brand')
+        element.click()
+        time.sleep(2)
+        currentURL = self.browser.current_url
+        self.assertEqual(currentURL, u'http://www.pocketchef.me/')
 
     def tearDown(self) :
         self.browser.quit()

00632479a2ed23cd5d669213cf009fec49ceed78
Aditya Prasad <aprasad101@utexas.edu>
2018-04-05 00:50:25 -0500

    jqtest2

---
+++ backend/jqtest2.js
@@ -0,0 +1,19 @@
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    var $ = require("jquery")(window);
+});
+
+var name_filter = [{"name": "name", "op": "like", "val": "%halal%"}];
+var cuisine_filter = [{"name": "cuisine", "op": "equals", "val": "American"}];
+
+$.ajax({
+  url: 'http://pocketchef.me/api/restaurants2',
+  data: {"q": JSON.stringify({"filters": name_filter})},
+  dataType: "json",
+  contentType: "application/json",
+  success: function(data) { console.log(data.objects); }
+});

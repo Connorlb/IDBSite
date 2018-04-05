17bc6c3d77b529e69912ef57a31223620c68162a
Aditya Prasad <aprasad101@utexas.edu>
2018-04-05 01:02:50 -0500

    jqtest2 attempt 5

--- backend/jqtest2.js
+++ backend/jqtest2.js
@@ -5,15 +5,15 @@ require('../pocketchef/node_modules/jsdom/lib/old-api').env("", function(err, wi
     }
 
     var $ = require('../pocketchef/node_modules/jquery')(window);
-});
 
-var name_filter = [{"name": "name", "op": "like", "val": "%halal%"}];
-var cuisine_filter = [{"name": "cuisine", "op": "equals", "val": "American"}];
+    var name_filter = [{"name": "name", "op": "like", "val": "%halal%"}];
+    var cuisine_filter = [{"name": "cuisine", "op": "equals", "val": "American"}];
 
-$.ajax({
-  url: 'http://pocketchef.me/api/restaurants2',
-  data: {"q": JSON.stringify({"filters": name_filter})},
-  dataType: "json",
-  contentType: "application/json",
-  success: function(data) { console.log(data.objects); }
+    $.ajax({
+      url: 'http://pocketchef.me/api/restaurants2',
+      data: {"q": JSON.stringify({"filters": name_filter})},
+      dataType: "json",
+      contentType: "application/json",
+      success: function(data) { console.log(data.objects); }
+    });
 });

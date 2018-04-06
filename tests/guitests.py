b4e426d91fd480e5f47c715cbb039145be980421
Aditya Prasad <aprasad101@utexas.edu>
2018-04-05 21:48:20 -0500

    Finished selenium tests

--- tests/guitests.py
+++ tests/guitests.py
@@ -2,6 +2,7 @@ import unittest
 import time
 from selenium import webdriver
 
+# Test home page html title element
 class Test1(unittest.TestCase) :
     def setUp(self) :
         self.browser = webdriver.Firefox()
@@ -15,6 +16,7 @@ class Test1(unittest.TestCase) :
     def tearDown(self) :
         self.browser.quit()
 
+# Test navbar logo hyperlink
 class Test2(unittest.TestCase) :
     def setUp(self) :
         self.browser = webdriver.Firefox()
@@ -26,6 +28,49 @@ class Test2(unittest.TestCase) :
         time.sleep(2)
         currentURL = self.browser.current_url
         self.assertEqual(currentURL, u'http://www.pocketchef.me/')
+
+    def tearDown(self) :
+        self.browser.quit()
+
+# Test carousel
+class Test3(unittest.TestCase) :
+    def setUp(self) :
+        self.browser = webdriver.Firefox()
+
+    def testCarousel(self) :
+        self.browser.get('http://www.pocketchef.me/')
+        item_elements = self.browser.find_elements_by_class_name("item")
+        active_item_elements = self.browser.find_elements_by_class_name("item active")
+        self.assertEqual(len(item_elements), 2)
+        self.assertEqual(len(active_item_elements), 1)
+
+    def tearDown(self) :
+        self.browser.quit()
+
+# Test about page team member biography elements
+class Test4(unittest.TestCase) :
+    def setUp(self) :
+        self.browser = webdriver.Firefox()
+
+    def testAbout(self) :
+        self.browser.get('http://www.pocketchef.me/about')
+        col_elements = self.browser.find_elements_by_class_name("col-sm-4 col-xs-12")
+        offset_col_elements = self.browser.find_elements_by_class_name("col-sm-4 col-sm-offset-2 col-xs-12")
+        self.assertEqual(len(col_elements), 4)
+        self.assertEqual(len(offset_col_elements), 1)
+
+    def tearDown(self) :
+        self.browser.quit()
+
+# Test API endpoint
+class Test4(unittest.TestCase) :
+    def setUp(self) :
+        self.browser = webdriver.Firefox()
+
+    def testApi(self) :
+        self.browser.get('http://www.pocketchef.me/api/restaurants2')
+        elements = self.browser.find_elements_by_tag_name('pre')
+        self.assertEqual(len(elements), 1)
 
     def tearDown(self) :
         self.browser.quit()

55438b7df46fae42b793617e272b3633917ad993
Jesus Romero <ejesusromero@gmail.com>
2018-04-14 20:10:42 -0500

    Starting on Search Page

---
+++ frontend/src/components/Search/Search.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+import SearchCard from './SearchCard'
+import Pagination from "react-js-pagination";
+import {Grid, Row, Col, Image, Button, MenuItem, DropdownButton} from 'react-bootstrap' ;
+import axios from 'axios'
+import React from 'react'
+
+
+class Search extends React.Component {
+    constructor(){
+
+    }
+    
+    return(){
+
+    }
+}

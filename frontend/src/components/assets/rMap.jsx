75fb603878d45fb846270ac9d1738157e0ad298a
Jesus Romero <ejesusromero@gmail.com>
2018-04-19 01:18:51 -0500

    F-E: Working on Restaurant Instance Appearance 1 - Test 2

--- frontend/src/components/assets/rMap.jsx
+++ frontend/src/components/assets/rMap.jsx
@@ -3,13 +3,17 @@ import GoogleMapReact from 'google-map-react';
 
  
 class rMap extends Component {
+    static defaultProps = {
+        center: {lat: 59.95, lng: 30.33},
+        zoom: 11
+      };
 
     render() {
         
         return (
             <GoogleMapReact
                 bootstrapURLKeys={{ key: "AIzaSyBFObWyqlbpObdkdNE0k4JwX9AB66cTGKw" }}
-                defaultCenter={{ center:{lat:this.props.latitude, lng:this.props.longitude }}}
+                defaultCenter={ this.props.center }
                 defaultZoom={this.props.zoom}
             >
             </GoogleMapReact>

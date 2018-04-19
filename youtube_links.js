f16d9994d358af63206d4575bc368d20525742a8
Aditya Prasad <aprasad101@utexas.edu>
2018-04-19 16:31:54 -0500

    Added cuisine youtube links

---
+++ youtube_links.js
@@ -0,0 +1,489 @@
+[
+	{
+		'American' : [
+			"https://www.youtube.com/watch?v=NxvO3nayMA0",
+			"https://www.youtube.com/watch?v=gZuDMKXWU_E&index=2&list=PLPXxNwplOM5ldwAbqubf4nwghu1wOU64N",
+			"https://www.youtube.com/watch?v=w8QjQL-LWGE&index=7&list=PLPXxNwplOM5ldwAbqubf4nwghu1wOU64N"
+		]
+	},
+
+	{
+		'Asian Fusion' : [
+			"https://www.youtube.com/watch?v=iSOKRRKT5mw",
+			"https://www.youtube.com/watch?v=DNTJnVVSi78",
+			"https://www.youtube.com/watch?v=r7jfStFlz54"
+		]
+	},
+
+	{
+		'Bagels' : [
+			"https://www.youtube.com/watch?v=tXXtXVv948A",
+			"https://www.youtube.com/watch?v=OOE5mjaH5r8",
+			"https://www.youtube.com/watch?v=GTaF7pxwiv0"
+		]
+	},
+
+	{
+		'Bakeries' : [
+			"https://www.youtube.com/watch?v=U0bUfUfceao",
+			"https://www.youtube.com/watch?v=xJ636Y8N6E8",
+			"https://www.youtube.com/watch?v=qyLLOctx7ZU"
+		]
+	},
+
+	{
+		'Barbeque' : [
+			"https://www.youtube.com/watch?v=5RJygAU7uNQ",
+			"https://www.youtube.com/watch?v=B4Srfo5_yOI",
+			"https://www.youtube.com/watch?v=vpS5mHJBQDE"
+		]
+	},
+
+	{
+		'Bars' : [
+			"https://www.youtube.com/watch?v=FSpxlvcw9d4",
+			"https://www.youtube.com/watch?v=1h-9DLb6jhw",
+			"https://www.youtube.com/watch?v=yO3U8DqsO3w"
+		]
+	},
+
+	{
+		'Beer' : [
+			"https://www.youtube.com/watch?v=RohjScZugxY",
+			"https://www.youtube.com/watch?v=nC4RIbrci5A",
+			"https://www.youtube.com/watch?v=qzSKcTzA4xE"
+		]
+	},
+
+	{
+		'Beer Bar' : [
+            "https://www.youtube.com/watch?v=RohjScZugxY",
+			"https://www.youtube.com/watch?v=nC4RIbrci5A",
+			"https://www.youtube.com/watch?v=qzSKcTzA4xE"
+		]
+	},
+
+	{
+		'Brazilian' : [
+			"https://www.youtube.com/watch?v=TItjC090Oe4",
+			"https://www.youtube.com/watch?v=1AVsJM20wsE",
+			"https://www.youtube.com/watch?v=auzJ4qLftBw"
+		]
+	},
+
+	{
+		'Breakfast & Brunch' : [
+			"https://www.youtube.com/watch?v=NxvO3nayMA0",
+			"https://www.youtube.com/watch?v=tGFp1wJrjuI",
+			"https://www.youtube.com/watch?v=4UP9IW1JvUk"
+		]
+	},
+
+	{
+		'Burgers' : [
+			"https://www.youtube.com/watch?v=iM_KMYulI_s",
+			"https://www.youtube.com/watch?v=y4y22RQH05c",
+			"https://www.youtube.com/watch?v=gZuDMKXWU_E"
+		]
+	},
+
+	{
+		'Cafes' : [
+			"https://www.youtube.com/watch?v=LVQ4MuA3vzs",
+			"https://www.youtube.com/watch?v=aEHBmi1927g",
+			"https://www.youtube.com/watch?v=SM_dqUSJvgk"
+		]
+	},
+
+	{
+		'Cajun' : [
+			"https://www.youtube.com/watch?v=LNIUNpgUKw4",
+			"https://www.youtube.com/watch?v=aVhEwLLiNEg",
+			"https://www.youtube.com/watch?v=Vx1yprdgs80"
+		]
+	},
+
+	{
+		'Caribbean' : [
+			"https://www.youtube.com/watch?v=y4m8xLqCfMY",
+			"https://www.youtube.com/watch?v=FgMFCkr7kYU",
+			"https://www.youtube.com/watch?v=8iyF-8IaFbY"
+		]
+	},
+
+	{
+		'Cheese' : [
+			"https://www.youtube.com/watch?v=ZUBPzAZSnDQ",
+			"https://www.youtube.com/watch?v=9CRggN5ZB6c",
+			"https://www.youtube.com/watch?v=Naz3lZXpNqo"
+		]
+	},
+
+	{
+		'Chicken Wings' : [
+			"https://www.youtube.com/watch?v=AUdt063QNhg",
+			"https://www.youtube.com/watch?v=BDVg0u2YQ7Q",
+			"https://www.youtube.com/watch?v=wL0gPxyD88U"
+		]
+	},
+
+	{
+		'Chinese' : [
+			"https://www.youtube.com/watch?v=hqWkaYZ6cWA",
+			"https://www.youtube.com/watch?v=4Qtmz1FDww0",
+			"https://www.youtube.com/watch?v=c0ICMcuhdPg"
+		]
+	},
+
+	{
+		'Cocktail Bars' : [
+            "https://www.youtube.com/watch?v=FSpxlvcw9d4",
+			"https://www.youtube.com/watch?v=1h-9DLb6jhw",
+			"https://www.youtube.com/watch?v=yO3U8DqsO3w"
+		]
+	},
+
+	{
+		'Coffee & Tea' : [
+            "https://www.youtube.com/watch?v=LVQ4MuA3vzs",
+			"https://www.youtube.com/watch?v=aEHBmi1927g",
+			"https://www.youtube.com/watch?v=SM_dqUSJvgk"
+		]
+	},
+
+	{
+		'Cuban' : [
+			"https://www.youtube.com/watch?v=u2MBqkRjOOs",
+			"https://www.youtube.com/watch?v=6Xo620bfUdw",
+			"https://www.youtube.com/watch?v=hXYoduN0kWs"
+		]
+	},
+
+	{
+		'Delis' : [
+			"https://www.youtube.com/watch?v=fO8S7HHoXqY",
+			"https://www.youtube.com/watch?v=b8d0gpsXzEk",
+			"https://www.youtube.com/watch?v=jwu2y9x5OlM"
+	},
+]
+
+	{
+		'Desserts' : [
+            "https://www.youtube.com/watch?v=U0bUfUfceao",
+			"https://www.youtube.com/watch?v=xJ636Y8N6E8",
+			"https://www.youtube.com/watch?v=qyLLOctx7ZU"
+		]
+	},
+
+	{
+		'Dim Sum' : [
+			"https://www.youtube.com/watch?v=Eh2yO3jpKNU",
+			"https://www.youtube.com/watch?v=v3rOKyNwxcM",
+			"https://www.youtube.com/watch?v=aGsS8AJwKG8"
+		]
+	},
+
+	{
+		'Diners' : [
+			"https://www.youtube.com/watch?v=7xDmCLP5mhY",
+			"https://www.youtube.com/watch?v=EM6_HAiUIlA",
+			"https://www.youtube.com/watch?v=NxvO3nayMA0"
+		]
+	},
+
+	{
+		'Donuts' : [
+			"https://www.youtube.com/watch?v=Gje8-r_Eg9I",
+			"https://www.youtube.com/watch?v=THlmlloXwpE",
+			"https://www.youtube.com/watch?v=-Bg5JaCQEi4"
+		]
+	},
+
+	{
+		'Ethiopian' : [
+			"https://www.youtube.com/watch?v=BQ5wsYu_VK0",
+			"https://www.youtube.com/watch?v=O_YBtz6aHDE",
+			"https://www.youtube.com/watch?v=LT3ExkWK6nE"
+		]
+	},
+
+	{
+		'European' : [
+			"https://www.youtube.com/watch?v=bjmYkPkjnVo",
+			"https://www.youtube.com/watch?v=nJzDfRIb9T0",
+			"https://www.youtube.com/watch?v=aBHXnbNk_qE"
+		]
+	},
+
+	{
+		'Food Stands' : [
+			"https://www.youtube.com/watch?v=lwu5xxqS0FU",
+			"https://www.youtube.com/watch?v=d-Hwbg0PrNk",
+			"https://www.youtube.com/watch?v=w_RNy2FQ_zw"
+		]
+	},
+
+	{
+		'Food Trucks' : [
+			"https://www.youtube.com/watch?v=ptRlbNtSGso",
+            "https://www.youtube.com/watch?v=lwu5xxqS0FU",
+			"https://www.youtube.com/watch?v=d-Hwbg0PrNk"
+		]
+	},
+
+	{
+		'French' : [
+			"https://www.youtube.com/watch?v=8HUlxam6X5Q",
+			"https://www.youtube.com/watch?v=-DLDMQucqDI",
+			"https://www.youtube.com/watch?v=qXPhVYpQLPA"
+		]
+	},
+
+	{
+		'German' : [
+			"https://www.youtube.com/watch?v=KZoxizWozCg",
+			"https://www.youtube.com/watch?v=RTfbOR16AR0",
+			"https://www.youtube.com/watch?v=UsjoGLSztic"
+		]
+	},
+
+	{
+		'Gluten-Free' : [
+			"https://www.youtube.com/watch?v=F4lrltBvchk",
+			"https://www.youtube.com/watch?v=xON-qmCl2cE",
+			"https://www.youtube.com/watch?v=ZHiYesEqF_c"
+		]
+	},
+
+	{
+		'Greek' : [
+			"https://www.youtube.com/watch?v=Z_GcsR7Q99s",
+			"https://www.youtube.com/watch?v=h6OSMbfhIao",
+			"https://www.youtube.com/watch?v=VO-yxB3oTOI"
+		]
+	},
+
+	{
+		'Hot Dogs' : [
+			"https://www.youtube.com/watch?v=d-Hwbg0PrNk",
+			"https://www.youtube.com/watch?v=y6wogkFWoIo",
+			"https://www.youtube.com/watch?v=IsV3IEWsfk8"
+		]
+	},
+
+	{
+		'Indian' : [
+			"https://www.youtube.com/watch?v=77YCB9TrROI",
+			"https://www.youtube.com/watch?v=hHPq2Tf2iyA",
+			"https://www.youtube.com/watch?v=WxEMQ75w7nE"
+		]
+	},
+
+	{
+		'Italian' : [
+			"https://www.youtube.com/watch?v=3AAdKl1UYZs",
+			"https://www.youtube.com/watch?v=Isr-BNpqwvY",
+			"https://www.youtube.com/watch?v=ldz6Fn-Qoi0"
+		]
+	},
+
+	{
+		'Japanese' : [
+			"https://www.youtube.com/watch?v=EizSoy7Ztto",
+			"https://www.youtube.com/watch?v=HtOmN8U21qI",
+			"https://www.youtube.com/watch?v=iBSKWTtzumg"
+		]
+	},
+
+	{
+		'Juice Bars & Smoothies' : [
+			"https://www.youtube.com/watch?v=CJN1n3fId_A",
+			"https://www.youtube.com/watch?v=yJqx-ZzRDbw",
+			"https://www.youtube.com/watch?v=YuDhbLQtt2k"
+		]
+	},
+
+	{
+		'Korean' : [
+			"https://www.youtube.com/watch?v=knxgR5esJu4",
+			"https://www.youtube.com/watch?v=jSh6VJYRV-g",
+			"https://www.youtube.com/watch?v=H_-0Z2S1XwQ"
+		]
+	},
+
+	{
+		'Latin American' : [
+			"https://www.youtube.com/watch?v=VmUyiRdAjnc",
+			"https://www.youtube.com/watch?v=_U5mW0BO8B8",
+			"https://www.youtube.com/watch?v=_U5mW0BO8B8"
+		]
+	},
+
+	{
+		'Lounges' : [
+            "https://www.youtube.com/watch?v=NxvO3nayMA0",
+			"https://www.youtube.com/watch?v=gZuDMKXWU_E&index=2&list=PLPXxNwplOM5ldwAbqubf4nwghu1wOU64N",
+			"https://www.youtube.com/watch?v=w8QjQL-LWGE&index=7&list=PLPXxNwplOM5ldwAbqubf4nwghu1wOU64N"
+		]
+	},
+
+	{
+		'Mediterranean' : [
+			"https://www.youtube.com/watch?v=hVpAjTkNzw4",
+			"https://www.youtube.com/watch?v=kfBixzl-ISI",
+			"https://www.youtube.com/watch?v=hHiNay53Iyc"
+		]
+	},
+
+	{
+		'Mexican' : [
+			"https://www.youtube.com/watch?v=LE2eNPH0dHA",
+			"https://www.youtube.com/watch?v=G2l8VkCPMkc",
+			"https://www.youtube.com/watch?v=TVZBpJa5GXE"
+		]
+	},
+
+	{
+		'Middle Eastern' : [
+			"https://www.youtube.com/watch?v=xuhC9DFxDNQ",
+			"https://www.youtube.com/watch?v=lMBddwbjQDI",
+			"https://www.youtube.com/watch?v=2ULRGUmb2yA"
+		]
+	},
+
+	{
+		'Pizza' : [
+			"https://www.youtube.com/watch?v=1-SJGQ2HLp8",
+			"https://www.youtube.com/watch?v=cxB4ACaaR4I",
+			"https://www.youtube.com/watch?v=RAOgl-Sv09U"
+		]
+	},
+
+	{
+		'Poke' : [
+			"https://www.youtube.com/watch?v=Zps_7GV3mlA",
+			"https://www.youtube.com/watch?v=bP2oAsOTPm8",
+			"https://www.youtube.com/watch?v=O_9HaMaR4GI"
+		]
+	},
+
+	{
+		'Pubs' : [
+            "https://www.youtube.com/watch?v=FSpxlvcw9d4",
+			"https://www.youtube.com/watch?v=1h-9DLb6jhw",
+			"https://www.youtube.com/watch?v=yO3U8DqsO3w"
+		]
+	},
+
+	{
+		'Ramen' : [
+			"https://www.youtube.com/watch?v=aafmrrx7Bh4",
+			"https://www.youtube.com/watch?v=EizSoy7Ztto",
+			"https://www.youtube.com/watch?v=8oeit_uJvXI"
+		]
+	},
+
+	{
+		'Sandwiches' : [
+            "https://www.youtube.com/watch?v=fO8S7HHoXqY",
+			"https://www.youtube.com/watch?v=b8d0gpsXzEk",
+			"https://www.youtube.com/watch?v=jwu2y9x5OlM"
+		]
+	},
+
+	{
+		'Seafood' : [
+			"https://www.youtube.com/watch?v=EziM5ckJbo4",
+			"https://www.youtube.com/watch?v=QtjuX5t1Qr0",
+			"https://www.youtube.com/watch?v=QGAJokcwBXI"
+		]
+	},
+
+	{
+		'Southern' : [
+			"https://www.youtube.com/watch?v=9HOPUyY2UOY",
+			"https://www.youtube.com/watch?v=UNsaFrgaQpI",
+			"https://www.youtube.com/watch?v=HGZqMi4HW60"
+		]
+	},
+
+	{
+		'Spanish' : [
+			"https://www.youtube.com/watch?v=L_dDUw_QuDU",
+			"https://www.youtube.com/watch?v=GmM-_sJmR6E",
+			"https://www.youtube.com/watch?v=fixHiTWKRWA"
+		]
+	},
+
+	{
+		'Special' : [
+            "https://www.youtube.com/watch?v=LVQ4MuA3vzs",
+			"https://www.youtube.com/watch?v=aEHBmi1927g",
+			"https://www.youtube.com/watch?v=ldz6Fn-Qoi0"
+		]
+	},
+
+	{
+		'Steakhouses' : [
+			"https://www.youtube.com/watch?v=JZVWkjFGCBI",
+			"https://www.youtube.com/watch?v=5uXIPhxL5XA",
+			"https://www.youtube.com/watch?v=PFDElc5sfSM"
+		]
+	},
+
+	{
+		'Sushi' : [
+			"https://www.youtube.com/watch?v=RrzXv14QNQA",
+			"https://www.youtube.com/watch?v=TbB1lqdrlkc",
+			"https://www.youtube.com/watch?v=HLQ16akzeI8"
+		]
+	},
+
+	{
+		'Tex-Mex' : [
+            "https://www.youtube.com/watch?v=LE2eNPH0dHA",
+			"https://www.youtube.com/watch?v=G2l8VkCPMkc",
+			"https://www.youtube.com/watch?v=TVZBpJa5GXE"
+		]
+	},
+
+	{
+		'Thai' : [
+			"https://www.youtube.com/watch?v=OV5fQ6EHFyk",
+			"https://www.youtube.com/watch?v=RoP3Zqfw3iw",
+			"https://www.youtube.com/watch?v=yRYoP1urSWY"
+		]
+	},
+
+	{
+		'Vegetarian' : [
+			"https://www.youtube.com/watch?v=tvtXoG_Re7s",
+			"https://www.youtube.com/watch?v=0w8EifC_yUo",
+			"https://www.youtube.com/watch?v=M6ol_t88T9Q"
+		]
+	},
+
+	{
+		'Vietnamese' : [
+			"https://www.youtube.com/watch?v=AJtMlTnqyw0",
+			"https://www.youtube.com/watch?v=5bIjDYEs6Qc",
+			"https://www.youtube.com/watch?v=iZJzTScgB-c"
+		]
+	},
+
+	{
+		'Vegan' : [
+			"https://www.youtube.com/watch?v=Ugwp8wSkVeQ",
+			"https://www.youtube.com/watch?v=B6vc6tJ9aTM",
+			"https://www.youtube.com/watch?v=3AUo5zja1-0"
+		]
+	},
+
+	{
+		'Wine Bars' : [
+			"https://www.youtube.com/watch?v=5JIhljxe7bE",
+			"https://www.youtube.com/watch?v=Z3LzCnQNi8Y",
+			"https://www.youtube.com/watch?v=Zqbo2Eo1stA"
+		]
+	}
+];
